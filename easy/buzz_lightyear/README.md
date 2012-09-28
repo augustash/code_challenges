e25e8a53cc8d67fdba65e274e11a100840670706
Josh Johnson <josh@augustash.com>
2012-09-28 01:14:57 -0500

    adds the buzz_lightyear challenge

---
+++ easy/buzz_lightyear/README.md
@@ -0,0 +1,3 @@
+## Buzz Lightyear Code Challenge 
+
+* Give all possible values of `x` where `x == x + 2` will evaluate to true
---
+++ easy/buzz_lightyear/php/buzz_lightyear.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Buzz Lightyear Code Challenge
+ *
+ * Give all possible values of `$x` where `$x == $x + 2;` will evaluate to `true`
+ */
+
+
+
+
+?>
