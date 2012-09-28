4f707699d5281089a1bfbe65c596da4c6a65248b
Dan Ficker <dan@da-man.com>
2012-09-28 11:17:06 -0500

    Added solutions for the Fibonacci in both JS and PHP.

--- easy/fibonacci_series/js/fibonacci_series.js
+++ easy/fibonacci_series/js/fibonacci_series.js
@@ -5,6 +5,26 @@
  */
 $(document).ready(function() {
 
-    // Put code here...
+    var sequence = [0, 1];
+    
+    // Load the sequence.
+    for (i = 2; i < 60; i++) {
+    	sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+	
+    // Load the input data.
+	input = $('#input pre').html();
+    lines = input.split("\n");
+    output = '';
+    
+    // Generate the output data.
+    for (i = 0; i < lines.length; i++) {
+    	value = lines[i];
+		// Check that it is a number, not gibberish.
+		if (!isNaN(parseFloat(value)) && isFinite(value)) {
+    		output += sequence[value] + '<br />' + "\n";
+    	}
+    }
+    $('#output').html(output);
 
 });
