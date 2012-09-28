414d398a10106efa5d0814004f3998707e30cb96
Josh Johnson <josh@augustash.com>
2012-09-28 00:44:18 -0500

    initial checkin of code challenges

---
+++ easy/reverse_words/README.md
@@ -0,0 +1,14 @@
+## Reverse Words Code Challenge 
+
+* Write a method to reverse the words of an input sentence. 
+* Reverse and print out each line's content as a new line to the console.
+
+__Example Input:__
+
+    Hello World
+    August Ash
+
+__Example Output:__
+
+    World Hello
+    Ash August
---
+++ easy/reverse_words/php/reverse_words.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Reverse Words Code Challenge
+ *
+ * Write a method to reverse the words of an input sentence. 
+ * Reverse and print out each line's content as a new line to the console.
+ *
+ *
+ * -- Example Input:
+ *
+ *  	Hello World
+ *  	August Ash
+ *
+ *
+ * -- Example Output:
+ *
+ *  	World Hello
+ *  	Ash August
+ *
+ * To access the text file you can either hard code it in here 
+ * or make it a command line argument like so:
+ *
+ * 		$ php reverse_words.php list.txt
+ *
+ * so $argv[1] would equal 'list.txt'
+ *
+ * $lines = file($argv[1]);
+ *
+ * OR
+ *
+ * $lines = file('list.txt');
+ */
+$lines = file($argv[1]);
+
+
+?>
