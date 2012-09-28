6f0b8d9922fb91f2c52484e085c34cfcf2c30aa5
Josh Johnson <josh@augustash.com>
2012-09-28 17:24:32 -0500

    found a better way to do the fibonacci series with a hash so it's uber ruby

--- easy/fibonacci_series/ruby/fibonacci_series.rb
+++ easy/fibonacci_series/ruby/fibonacci_series.rb
@@ -1,41 +1,35 @@
 # == Fibonacci Series Code Challenge
 #
-# The Fibonacci series is defined as: 
-# 
-#   F(0) = 0; F(1) = 1; F(n) = F(n-1) + F(n-2) when n > 1; 
-# 
+# The Fibonacci series is defined as:
+#
+#   F(0) = 0; F(1) = 1; F(n) = F(n-1) + F(n-2) when n > 1;
+#
 # Given a positive integer 'n', print out the F(n).
-# 
+#
 # -- Example Input:
-# 
+#
 #   5
 #   12
-# 
-# 
+#
+#
 # -- Example Output:
-# 
+#
 #   5
 #   144
 #
 #
 # ARGV[0] should be the text file you pass in when you call this script
-# 
+#
 # -- Example:
-# 
+#
 #   $ ruby fibonacci_series.rb list.txt
 #
 require './fibber.rb'
 
 File.open(ARGV[0]).each_line do |n|
   # begin coding here
-  fibber = Fibber.new
 
-  puts "Finding Fibonacci value at position #{n.to_i}"
-
-  puts "using Fibber#fib_array(n)"
-  puts fibber.fib_array(n)
-
-  puts "using Fibber#fib_iterative(n)"
-  puts fibber.fib_iterative(n)
+  fibonacci = Hash.new{ |h,k| h[k] = k < 2 ? k : h[k-1] + h[k-2] }
+  puts fibonacci[n.to_i]
 
 end
