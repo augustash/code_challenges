eca9893d12a5d6aa75bf9ec0ef34a74231f5b7c7
Josh Johnson <josh@augustash.com>
2012-09-28 21:30:44 -0500

    add time tracking and one-line the sucka

--- easy/buzz_lightyear/ruby/buzz_lightyear.rb
+++ easy/buzz_lightyear/ruby/buzz_lightyear.rb
@@ -2,8 +2,8 @@
 #
 # Give all possible values of `x` where `x == x + 2` will evaluate to `true`
 
-values = [ 1.0/0, -1.0/0, 1e17 ]
+start_time = Time.now
 
-values.each do |x|
-  puts "#{x} evaluates to true" if x == x + 2
-end
+[1.0/0, -1.0/0, 1e17].each { |x| p "#{x} evaluates to true" if x == x + 2 }
+
+puts "\nCompleted in #{Time.now - start_time} ms"
