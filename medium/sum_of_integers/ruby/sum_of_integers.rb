208d6c2b00e1fdd1193ea620fbb00c3a7c930dd0
Josh Johnson <josh@augustash.com>
2012-11-09 10:51:49 -0600

    finish ruby version of sum_of_integers

--- medium/sum_of_integers/ruby/sum_of_integers.rb
+++ medium/sum_of_integers/ruby/sum_of_integers.rb
@@ -1,31 +1,45 @@
 # == Sum of Integers
 #
-# Write a program to determine the largest sum of contiguous integers 
-# in a list. In other words, of all the possible contiguous subarrays 
+# Write a program to determine the largest sum of contiguous integers
+# in a list. In other words, of all the possible contiguous subarrays
 # for a given array, find the one with the largest sum, and print that sum.
-# 
-# 
-# Your program should accept a textfile as its first argument. This file 
-# will a comma separated list of integers, one set per line. e.g. 
-# 
+#
+#
+# Your program should accept a textfile as its first argument. This file
+# will a comma separated list of integers, one set per line. e.g.
+#
 # -- Example Input:
-# 
+#
 # 	-10, 2, 3, -2, 0, 5, -15
 # 	2,3,-2,-1,10
-# 
-# 
+#
+#
 # -- Example Output:
-# 
+#
 # 	8
 # 	12
 #
 #
 # ARGV[0] should be the text file you pass in when you call this script
-# 
+#
 # -- Example:
-# 
+#
 #   $ ruby sum_of_integers.rb list.txt
-# 
+#
+#
+# -- ANSWER SHOULD BE:
+#
+#   23
+#   22
+#
 File.open(ARGV[0]).each_line do |line|
   # begin coding here
+  max_so_far      = 0
+  max_ending_here = 0
+
+  line.split(',').each do |n|
+    max_ending_here   = [0, max_ending_here + n.to_i].max
+    max_so_far        = [max_ending_here, max_so_far].max
+  end
+  puts max_so_far
 end
